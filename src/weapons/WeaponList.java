f9cf16776cb619446307b5f2ca4df569db2e08ba
gregoryneal <gregcraig23@gmail.com>
2015-07-01 21:42:49 -0500

    rehaul of weapons enumerations, include all relevant weapon info

--- src/weapons/WeaponList.java
+++ src/weapons/WeaponList.java
@@ -1,69 +1,166 @@
 package weapons;
 
 import weapons.WeaponTypes;
+import damage.DamageTypes;
+import dice.Dice;
 
 public enum WeaponList {
-	CLUB(WeaponTypes.SIMPLE_MELEE),
-	DAGGER(WeaponTypes.SIMPLE_MELEE),
-	GREATCLUB(WeaponTypes.SIMPLE_MELEE),
-	HANDAXE(WeaponTypes.SIMPLE_MELEE),
-	JAVELIN(WeaponTypes.SIMPLE_MELEE),
-	LIGHT_HAMMER(WeaponTypes.SIMPLE_MELEE),
-	MACE(WeaponTypes.SIMPLE_MELEE),
-	QUARTERSTAFF(WeaponTypes.SIMPLE_MELEE),
-	SICKLE(WeaponTypes.SIMPLE_MELEE),
-	SPEAR(WeaponTypes.SIMPLE_MELEE),
-	UNARMED_STRIKE(WeaponTypes.SIMPLE_MELEE),
-	CROSSBOW_LIGHT(WeaponTypes.SIMPLE_RANGED),
-	DART(WeaponTypes.SIMPLE_RANGED),
-	SHORTBOW(WeaponTypes.SIMPLE_RANGED),
-	SLING(WeaponTypes.SIMPLE_RANGED),
-	BATTLEAXE(WeaponTypes.MARTIAL_MELEE),
-	FLAIL(WeaponTypes.MARTIAL_MELEE),
-	GLAIVE(WeaponTypes.MARTIAL_MELEE),
-	GREATAXE(WeaponTypes.MARTIAL_MELEE),
-	GREATSWORD(WeaponTypes.MARTIAL_MELEE),
-	HALBERD(WeaponTypes.MARTIAL_MELEE),
-	LANCE(WeaponTypes.MARTIAL_MELEE),
-	LONGSWORD(WeaponTypes.MARTIAL_MELEE),
-	MAUL(WeaponTypes.MARTIAL_MELEE),
-	MORNINGSTAR(WeaponTypes.MARTIAL_MELEE),
-	PIKE(WeaponTypes.MARTIAL_MELEE),
-	RAPIER(WeaponTypes.MARTIAL_MELEE),
-	SCIMITAR(WeaponTypes.MARTIAL_MELEE),
-	SHORTSWORD(WeaponTypes.MARTIAL_MELEE),
-	TRIDENT(WeaponTypes.MARTIAL_MELEE),
-	WAR_PICK(WeaponTypes.MARTIAL_MELEE),
-	WARHAMMER(WeaponTypes.MARTIAL_MELEE),
-	WHIP(WeaponTypes.MARTIAL_MELEE),
-	BLOWGUN(WeaponTypes.MARTIAL_RANGED),
-	CROSSBOW_HAND(WeaponTypes.MARTIAL_RANGED),
-	CROSSBOW_HEAVY(WeaponTypes.MARTIAL_RANGED),
-	LONGBOW(WeaponTypes.MARTIAL_RANGED),
-	NET(WeaponTypes.MARTIAL_RANGED);
+	//WeaponList	(WeaponTypes,       		isLight,	isFinesse,	isThrown, 	isTwoHanded,	isVersatile, 	usesAmmo,	isLoading, 	isHeavy, 	isReach, 	isSpecial,	versatileDie,	damageDie,		damageType,					weight)
+	CLUB			(WeaponTypes.SIMPLE_MELEE, 	true,    	false,    	false,   	false,       	false,       	false,      false,    	false,   	false,   	false,		Dice.NULL,		new Dice(1,4),	DamageTypes.BLUDGEONING,	2),
+	DAGGER			(WeaponTypes.SIMPLE_MELEE, 	true, 	 	true,  		true,  		false,			false,			false,		false, 		false, 		false,		false,		Dice.NULL,		new Dice(1,4), 	DamageTypes.PIERCING,		1),
+	GREATCLUB		(WeaponTypes.SIMPLE_MELEE,	false,		false,		false,		true,			false,			false,		false,		false,		false,		false,		Dice.NULL,		new Dice(1,8),	DamageTypes.BLUDGEONING,	10),
+	HANDAXE			(WeaponTypes.SIMPLE_MELEE,	true,		false,		true,		false,			false,			false,		false,		false,		false,		false,		Dice.NULL,		new Dice(1,6),	DamageTypes.SLASHING,		2),
+	JAVELIN			(WeaponTypes.SIMPLE_MELEE,	false,		false,		true,		false,			false,			false,		false,		false,		false,		false,		Dice.NULL,		new Dice(1,6), 	DamageTypes.PIERCING,		2),
+	LIGHT_HAMMER	(WeaponTypes.SIMPLE_MELEE,	true,		false,		true,		false,			false,			false,		false,		false,		false,		false,		Dice.NULL,		new Dice(1,4), 	DamageTypes.BLUDGEONING,	2),
+	MACE			(WeaponTypes.SIMPLE_MELEE,	false,		false,		false,		false,			false,			false,		false,		false,		false,		false,		Dice.NULL,		new Dice(1,6),	DamageTypes.BLUDGEONING,	4),
+	QUARTERSTAFF	(WeaponTypes.SIMPLE_MELEE,	false,		false,		false,		false,			true,			false,		false,		false,		false,		false,		new Dice(1,8),	new Dice(1,6), 	DamageTypes.BLUDGEONING,	4),
+	SICKLE			(WeaponTypes.SIMPLE_MELEE,	true,		false,		false,		false,			false,			false,		false,		false,		false,		false,		Dice.NULL,		new Dice(1,4),	DamageTypes.SLASHING,		2),
+	SPEAR			(WeaponTypes.SIMPLE_MELEE,	false,		false,		true,		false,			true,			false,		false,		false,		false,		false,		new Dice(1,8),	new Dice(1,6),	DamageTypes.BLUDGEONING,	3),
+	UNARMED_STRIKE	(WeaponTypes.SIMPLE_MELEE,	false,		false,		false,		false,			false,			false,		false,		false,		false,		false,		Dice.NULL,		new Dice(1,1),	DamageTypes.BLUDGEONING,	0),
+	CROSSBOW_LIGHT	(WeaponTypes.SIMPLE_RANGED,	false,		false,		false,		true,			false,			true,		true,		false,		false,		false,		Dice.NULL,		new Dice(1,8),	DamageTypes.PIERCING,		5),
+	DART			(WeaponTypes.SIMPLE_RANGED,	false,		true,		true,		false,			false,			false,		false,		false,		false,		false,		Dice.NULL,		new Dice(1,4),	DamageTypes.PIERCING,		.25),
+	SHORTBOW		(WeaponTypes.SIMPLE_RANGED,	false,		false,		false,		true,			false,			true,		false,		false,		false,		false,		Dice.NULL,		new Dice(1,6),	DamageTypes.PIERCING,		2),
+	SLING			(WeaponTypes.SIMPLE_RANGED,	false,		false,		false,		false,			false,			true,		false,		false,		false,		false,		Dice.NULL,		new Dice(1,4),	DamageTypes.BLUDGEONING,	0),
+	BATTLEAXE		(WeaponTypes.MARTIAL_MELEE,	false,		false,		false,		false,			true,			false,		false,		false,		false,		false,		new Dice(1,10),	new Dice(1,8), 	DamageTypes.SLASHING,		4),
+	FLAIL			(WeaponTypes.MARTIAL_MELEE,	false,		false,		false,		false,			false,			false,		false,		false,		false,		false,		Dice.NULL,		new Dice(1,8),	DamageTypes.BLUDGEONING,	2),
+	GLAIVE			(WeaponTypes.MARTIAL_MELEE, false,		false,		false,		true,			false,			false,		false,		true,		true,		false,		Dice.NULL,		new Dice(1,10),	DamageTypes.SLASHING,	 	6),
+	GREATAXE		(WeaponTypes.MARTIAL_MELEE,	false,		false,		false,		true,			false,			false,		false,		true,		false,		false,		Dice.NULL,		new Dice(1,12),	DamageTypes.SLASHING,		7),
+	GREATSWORD		(WeaponTypes.MARTIAL_MELEE,	false,		false,		false,		true,			false,			false,		false,		true,		false,		false,		Dice.NULL,		new Dice(2,6),	DamageTypes.SLASHING,		6),
+	HALBERD			(WeaponTypes.MARTIAL_MELEE,	false,		false,		false,		true,			false,			false,		false,		true,		true,		false,		Dice.NULL,		new Dice(1,10),	DamageTypes.SLASHING,		6),
+	LANCE			(WeaponTypes.MARTIAL_MELEE,	false,		false,		false,		false,			false,			false,		false,		false,		true,		true,		Dice.NULL,		new Dice(1,12),	DamageTypes.PIERCING,		6),
+	LONGSWORD		(WeaponTypes.MARTIAL_MELEE,	false,		false,		false,		false,			true,			false,		false,		false,		false,		false,		new Dice(1,10), new Dice(1,8), 	DamageTypes.SLASHING,		3),
+	MAUL			(WeaponTypes.MARTIAL_MELEE,	false,		false,		false,		true,			false,			false,		false,		true,		false,		false,		Dice.NULL,		new Dice(2,6),	DamageTypes.BLUDGEONING,	10),
+	MORNINGSTAR		(WeaponTypes.MARTIAL_MELEE,	false,		false,		false,		false,			false,			false,		false,		false,		false,		false,		Dice.NULL,		new Dice(1,8), 	DamageTypes.PIERCING,		4),
+	PIKE			(WeaponTypes.MARTIAL_MELEE,	false,		false,		false,		true,			false,			false,		false,		true,		true,		false,		Dice.NULL,		new Dice(1,10),	DamageTypes.PIERCING,		18),
+	RAPIER			(WeaponTypes.MARTIAL_MELEE,	false,		true,		false,		false,			false,			false,		false,		false,		false,		false,		Dice.NULL,		new Dice(1,8),	DamageTypes.PIERCING,		2),
+	SCIMITAR		(WeaponTypes.MARTIAL_MELEE,	true,		true,		false,		false,			false,			false,		false,		false,		false,		false,		Dice.NULL,		new Dice(1,6), 	DamageTypes.SLASHING,		3),
+	SHORTSWORD		(WeaponTypes.MARTIAL_MELEE,	true,		true,		false,		false,			false,			false,		false,		false,		false,		false,		Dice.NULL,		new Dice(1,6),	DamageTypes.PIERCING,		2),
+	TRIDENT			(WeaponTypes.MARTIAL_MELEE,	false,		false,		true,		false,			true,			false,		false,		false,		false,		false,		new Dice(1,8), 	new Dice(1,6), 	DamageTypes.PIERCING,		4),
+	WAR_PICK		(WeaponTypes.MARTIAL_MELEE,	false,		false,		false,		false,			false,			false,		false,		false,		false,		false,		Dice.NULL,		new Dice(1,8),	DamageTypes.PIERCING,		2),
+	WARHAMMER		(WeaponTypes.MARTIAL_MELEE,	false,		false,		false,		false,			true,			false,		false,		false,		false,		false,		new Dice(1,10),	new Dice(1,8),	DamageTypes.BLUDGEONING,	2),
+	WHIP			(WeaponTypes.MARTIAL_MELEE, false,		true,		false,		false,			false,			false,		false,		false,		true,		false,		Dice.NULL,		new Dice(1,4),	DamageTypes.SLASHING,		3),
+	BLOWGUN			(WeaponTypes.MARTIAL_RANGED,false,		false,		false,		false,			false,			true,		true,		false,		false,		false,		Dice.NULL,		new Dice(1,1),	DamageTypes.PIERCING,		1),
+	CROSSBOW_HAND	(WeaponTypes.MARTIAL_RANGED,true,		false,		false,		false,			false,			true,		true,		false,		false,		false,		Dice.NULL,		new Dice(1,6),	DamageTypes.PIERCING,		3),
+	CROSSBOW_HEAVY	(WeaponTypes.MARTIAL_RANGED,false,		false,		false,		true,			false,			true,		true,		true,		false,		false,		Dice.NULL,		new Dice(1,10),	DamageTypes.PIERCING,		18),
+	LONGBOW			(WeaponTypes.MARTIAL_RANGED,false,		false,		false,		true,			false,			true,		false,		true,		false,		false,		Dice.NULL,		new Dice(1,8),	DamageTypes.PIERCING,		2),
+	NET				(WeaponTypes.MARTIAL_RANGED,false,		false,		true,		false,			false,			false,		false,		false,		false,		true,		Dice.NULL,		Dice.NULL,		DamageTypes.NONE,			3);
 	
-	private final WeaponTypes value;
+	private final WeaponTypes wt;
+	private final DamageTypes dt;
+	private final boolean isLight;
+	private final boolean isFinesse;
+	private final boolean isThrown;
+	private final boolean isTwoHanded;
+	private final boolean isVersatile;
+	private final boolean usesAmmo;
+	private final boolean isLoading;
+	private final boolean isHeavy;
+	private final boolean isReach;
+	private final boolean isSpecial;
+	private final Dice damageDie;
+	private final Dice versatileDie;
+	private final double weight;
 	
-	private WeaponList(WeaponTypes value)
+	private WeaponList(WeaponTypes value, boolean light, boolean finesse, boolean thrown, boolean twoHanded, boolean versatile, boolean ammo, boolean loading, boolean heavy, boolean reach, boolean special, Dice vDie, Dice dDie, DamageTypes damType, double w)
 	{
-		this.value = value;
+		this.wt = value;
+		this.dt = damType;
+		this.isLight = light;
+		this.isFinesse = finesse;
+		this.isThrown = thrown;
+		this.isTwoHanded = twoHanded;
+		this.isVersatile = versatile;
+		this.versatileDie = vDie;
+		this.damageDie = dDie;
+		this.usesAmmo = ammo;
+		this.isLoading = loading;
+		this.isHeavy = heavy;
+		this.isReach = reach;
+		this.isSpecial = special;
+		this.weight = w;
+	}
+	
+	public Dice damageDie()
+	{
+		return this.damageDie;
+	}
+	
+	public Dice versatileDie()
+	{
+		return this.versatileDie;
+	}
+	
+	public boolean usesAmmo()
+	{
+		return this.usesAmmo;
+	}
+	
+	public boolean isLoading()
+	{
+		return this.isLoading;
+	}
+	
+	public boolean isHeavy()
+	{
+		return this.isHeavy;
+	}
+	
+	public boolean isReach()
+	{
+		return this.isReach;
+	}
+	
+	public boolean isSpecial()
+	{
+		return this.isSpecial;
+	}
+	
+	public double weight()
+	{
+		return this.weight;
 	}
 	
 	public WeaponTypes getWeaponType()
 	{
-		return value;
+		return wt;
 	}
 	
 	//finesse weapons allow you to use your DEX mod instead of your STR mod for hit and damage rolls
-	public boolean isFinesse(WeaponList weapon)
+	public boolean isFinesse()
 	{
-		if(weapon == WeaponList.DAGGER | weapon == WeaponList.DART | weapon == WeaponList.RAPIER | weapon == WeaponList.SCIMITAR | weapon == WeaponList.SHORTSWORD | weapon == WeaponList.WHIP)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
+		return this.isFinesse;
 	}
+	
+	public String properties()
+	{
+		return "";
+	}
+	
+	public boolean isLight()
+	{
+		
+		return this.isLight;
+	}
+	
+	public boolean isThrown()
+	{
+		
+		return this.isThrown;
+	}
+	
+	public boolean isTwoHanded()
+	{		
+		return this.isTwoHanded;
+	}
+	
+	public boolean isVersatile()
+	{
+		return this.isVersatile;
+	}
+	
+	public DamageTypes damageType()
+	{
+		return this.dt;
+	}
+	
 }
