53a33bb2c8294dc592b34d76401df87e3469739e
gregoryneal <gregcraig23@gmail.com>
2015-07-01 21:41:00 -0500

    add damage type enumerations

---
+++ src/damage/DamageTypes.java
@@ -0,0 +1,19 @@
+package damage;
+
+public enum DamageTypes {
+	//page 180 of phb
+	ACID,
+	BLUDGEONING,
+	COLD,
+	FIRE,
+	FORCE,
+	LIGHTNING,
+	NECROTIC,
+	PIERCING,
+	POISON,
+	PSYCHIC,
+	RADIANT,
+	SLASHING,
+	THUNDER,
+	NONE;
+}
