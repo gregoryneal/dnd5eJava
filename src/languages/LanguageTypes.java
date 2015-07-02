189f6344c720510092613aecbb3160a5130e35e9
gregoryneal <gregcraig23@gmail.com>
2015-07-01 19:06:49 -0500

    spaced enum values to make a nice column :)

--- src/languages/LanguageTypes.java
+++ src/languages/LanguageTypes.java
@@ -3,21 +3,21 @@ package languages;
 import languages.LanguageScriptTypes;
 
 public enum LanguageTypes {
-	COMMON(LanguageScriptTypes.COMMON),
-	DWARVISH(LanguageScriptTypes.DWARVISH),
-	ELVISH(LanguageScriptTypes.ELVISH),
-	GIANT(LanguageScriptTypes.DWARVISH),
-	GNOMISH(LanguageScriptTypes.DWARVISH),
-	GOBLIN(LanguageScriptTypes.DWARVISH),
-	HALFLING(LanguageScriptTypes.COMMON),
-	ORC(LanguageScriptTypes.DWARVISH),
-	ABYSSAL(LanguageScriptTypes.INFERNAL),
-	CELESTIAL(LanguageScriptTypes.CELESTIAL),
-	DRACONIC(LanguageScriptTypes.DRACONIC),
-	DEEP_SPEECH(LanguageScriptTypes.NONE),
-	INFERNAL(LanguageScriptTypes.INFERNAL),
-	PRIMORDIAL(LanguageScriptTypes.DWARVISH),
-	SYLVAN(LanguageScriptTypes.ELVISH);
+	COMMON		(LanguageScriptTypes.COMMON),
+	DWARVISH	(LanguageScriptTypes.DWARVISH),
+	ELVISH		(LanguageScriptTypes.ELVISH),
+	GIANT		(LanguageScriptTypes.DWARVISH),
+	GNOMISH		(LanguageScriptTypes.DWARVISH),
+	GOBLIN		(LanguageScriptTypes.DWARVISH),
+	HALFLING	(LanguageScriptTypes.COMMON),
+	ORC			(LanguageScriptTypes.DWARVISH),
+	ABYSSAL		(LanguageScriptTypes.INFERNAL),
+	CELESTIAL	(LanguageScriptTypes.CELESTIAL),
+	DRACONIC	(LanguageScriptTypes.DRACONIC),
+	DEEP_SPEECH	(LanguageScriptTypes.NONE),
+	INFERNAL	(LanguageScriptTypes.INFERNAL),
+	PRIMORDIAL	(LanguageScriptTypes.DWARVISH),
+	SYLVAN		(LanguageScriptTypes.ELVISH);
 	
 	
 	final LanguageScriptTypes value;
