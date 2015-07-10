3f32e92235c780068e29583fe2bf52ec19df2799
gregoryneal <gregcraig23@gmail.com>
2015-07-10 13:32:27 -0500

    Add inventory class to handle character inventory

---
+++ src/equipment/Inventory.java
@@ -0,0 +1,15 @@
+package equipment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inventory
+{
+	List<Object> inv = new ArrayList<Object>();
+	
+	public Inventory()
+	{
+		
+	}
+
+}
