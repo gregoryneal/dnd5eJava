c3606114b3f2bff309769939eb454a837b9750b1
gregoryneal <gregcraig23@gmail.com>
2015-07-11 13:16:25 -0500

    Add class enumerations for common multiclass requirements

--- src/classes/MulticlassRequirement.java
+++ src/classes/MulticlassRequirement.java
@@ -2,21 +2,31 @@ package classes;
 
 import java.io.Serializable;
 
+import abilities.AbilityTypes;
+
 public class MulticlassRequirement implements Serializable 
 {
 
 	private static final long serialVersionUID = 1630435957428011060L;
 	
-	private ClassTypes classReq;
+	private AbilityTypes classReq;
 	private int levelReq;
+	
+	public static MulticlassRequirement NULL = new MulticlassRequirement(AbilityTypes.NONE,0);
+	public static MulticlassRequirement STR = new MulticlassRequirement(AbilityTypes.STRENGTH,13);
+	public static MulticlassRequirement DEX = new MulticlassRequirement(AbilityTypes.DEXTERITY,13);
+	public static MulticlassRequirement CON = new MulticlassRequirement(AbilityTypes.CONSTITUTION,13);
+	public static MulticlassRequirement INT = new MulticlassRequirement(AbilityTypes.INTELLIGENCE,13);
+	public static MulticlassRequirement WIS = new MulticlassRequirement(AbilityTypes.WISDOM,13);
+	public static MulticlassRequirement CHA = new MulticlassRequirement(AbilityTypes.CHARISMA,13);
 
-	public MulticlassRequirement(ClassTypes classType, int levelRequired) 
+	public MulticlassRequirement(AbilityTypes classToCheck, int requiredLevel) 
 	{
-		this.classReq = classType;
-		this.levelReq = levelRequired;
+		this.classReq = classToCheck;
+		this.levelReq = requiredLevel;
 	}
 	
-	public ClassTypes getClassType()
+	public AbilityTypes getClassType()
 	{
 		return this.classReq;
 	}
