31cab42b4f07f6e4b9727af18aeaa665d4c6902d
gregoryneal <gregcraig23@gmail.com>
2015-07-11 13:17:02 -0500

    Add constructor to enum that specifies the requirement to multiclass into this class

--- src/classes/ClassTypes.java
+++ src/classes/ClassTypes.java
@@ -1,24 +1,41 @@
 package classes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum ClassTypes {
-	NONE,
-	BARBARIAN,
-	BARD,
-	CLERIC,
-	DRUID,
-	FIGHTER,
-	MONK,
-	PALADIN,
-	RANGER,
-	ROGUE,
-	SORCERER,
-	WARLOCK,
-	WIZARD;
+	NONE(MulticlassRequirement.NULL,MulticlassRequirement.NULL,""),
+	BARBARIAN(MulticlassRequirement.STR,MulticlassRequirement.NULL,""),
+	BARD(MulticlassRequirement.CHA,MulticlassRequirement.NULL,""),
+	CLERIC(MulticlassRequirement.WIS,MulticlassRequirement.NULL,""),
+	DRUID(MulticlassRequirement.WIS,MulticlassRequirement.NULL,""),
+	FIGHTER(MulticlassRequirement.STR,MulticlassRequirement.DEX,"OR"),
+	MONK(MulticlassRequirement.DEX,MulticlassRequirement.WIS,"AND"),
+	PALADIN(MulticlassRequirement.STR,MulticlassRequirement.CHA,"AND"),
+	RANGER(MulticlassRequirement.DEX,MulticlassRequirement.WIS,"AND"),
+	ROGUE(MulticlassRequirement.DEX,MulticlassRequirement.NULL,""),
+	SORCERER(MulticlassRequirement.CHA,MulticlassRequirement.NULL,""),
+	WARLOCK(MulticlassRequirement.CHA,MulticlassRequirement.NULL,""),
+	WIZARD(MulticlassRequirement.INT,MulticlassRequirement.NULL,"");
 	
 	@Override
 	public String toString()
 	{
 		String str = this.name();
 		return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
+	}
+	
+	public final List<Object> reqs = new ArrayList<>();
+	
+	private ClassTypes(MulticlassRequirement multiclassReq1, MulticlassRequirement multiclassReq2, String andOR)
+	{
+		this.reqs.add(multiclassReq1);
+		
+		//they only need one skill at a certain lvl
+		if(!(multiclassReq2.equals(MulticlassRequirement.NULL) && andOR.equals("")))
+		{
+			this.reqs.add(multiclassReq2);
+			this.reqs.add(andOR);
+		}
 	}
 }
